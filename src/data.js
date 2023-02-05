c7614f363cf1bdd0e714c426af6226ea9aae9c42
lixiao54 <lixiao54@meituan.com>
2023-02-05 10:10:52 +0800

    feat: update hero pool

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const HeroType = {
+export const HeroType = {
     1: {
         name: '普通',
         color: 'green'
@@ -21,7 +21,7 @@ const HeroType = {
     }
 } 
 
-export const HeroPool = [
+const Pool1 = [
     {
         name: '蒙奇D路飞',
         type: 1,
@@ -33,7 +33,7 @@ export const HeroPool = [
         need: []
     },
     {
-        name: '罗罗亚索索隆',
+        name: '罗罗诺索隆',
         type: 1,
         need: []
     },
@@ -69,3 +69,80 @@ export const HeroPool = [
         need: []
     },
 ]
+
+const Pool2 = [
+    {
+        name: '波特卡斯D艾斯',
+        type: 2,
+        need: ['蒙奇D路飞', '海军炮兵']
+    },
+    {
+        name: '尼可罗宾',
+        type: 2,
+        need: ['娜美', '山治']
+    },
+    {
+        name: '布鲁克',
+        type: 2,
+        need: ['罗罗诺索隆', '托尼托尼乔巴']
+    },
+    {
+        name: '狙击王乌索普',
+        type: 2,
+        need: ['乌索普', '乌索普']
+    },
+    {
+        name: '弗兰奇',
+        type: 2,
+        need: ['乌索普', '蒙奇D路飞']
+    },
+    {
+        name: 'CP9猫头鹰',
+        type: 2,
+        need: ['海军剑兵', '海军剑兵']
+    },
+    {
+        name: '达斯琪',
+        type: 2,
+        need: ['海军剑兵', '海军炮兵']
+    },
+    {
+        name: '小八',
+        type: 2,
+        need: ['海军炮兵', '娜美']
+    },
+    {
+        name: '佩罗娜',
+        type: 2,
+        need: ['巴基', '娜美']
+    },
+    {
+        name: '依玛祖娜',
+        type: 2,
+        need: ['山治', '罗罗诺索隆']
+    },
+    {
+        name: '培波',
+        type: 2,
+        need: ['托尼托尼乔巴', '蒙奇D路飞']
+    },
+    {
+        name: '托尼托尼乔巴蓝波球强化',
+        type: 2,
+        need: ['托尼托尼乔巴', '托尼托尼乔巴']
+    },
+]
+
+const Pool3 = [
+    
+]
+
+const Pool4 = [
+    
+]
+
+const Pool5 = [
+    
+]
+
+export const zHeroPool = [...Pool1, ...Pool2, ...Pool3, ...Pool4, ...Pool5];
