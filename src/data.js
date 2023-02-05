55bbbd984f096abb54816f06e620f351b87088eb
lixiao54 <lixiao54@meituan.com>
2023-02-05 09:47:42 +0800

    chore: 项目搭建

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const HeroType = {
+    1: {
+        name: '普通',
+        color: 'green'
+    },
+    2: {
+        name: '罕见',
+        color: 'purple'
+    },
+    3: {
+        name: '特别'
+
+    }
+} 
+
+export const HeroPool = [
+    {
+        name: 
+    }
+]
